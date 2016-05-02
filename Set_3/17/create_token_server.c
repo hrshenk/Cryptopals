e1633bb255a8717783c7a6703b5bfaec4943d1aa
Hayden Shenk <Hayden.Shenk@gmail.com>
2016-05-02 17:07:58 +0000

    Updated create_token_server to write the encrypted token to a file.

--- Set_3/17/create_token_server.c
+++ Set_3/17/create_token_server.c
@@ -11,14 +11,14 @@ int create_token_server()
     const char* strings[10];
     strings_init(strings);  //fills the input array with pointers to the strings provided for this challenge.
     
-    const char *encoded_string = strings[(rand() % 10)]; //select one of the strings at random
+    const char *encoded_string = strings[(rand() % 10)]; //select one of the provided strings at random
     char *decoded_string = (char *)calloc(strlen(encoded_string), sizeof(char));
     int decoded_len = base64_decode(encoded_string, decoded_string);  //decode the selected string
     
     generate_key(key);//generate a 16 byte key to use for encryption and decryption.
     unsigned char iv[16];
     generate_key(iv);  //generate an initialization vector
-    //allocate memory for the ciphertext
+    //allocate memory for iv|ciphertext 
     unsigned char *ciphertext = (unsigned char *) calloc(decoded_len + 32, sizeof(char));
     //encrypt the selected string.  note, encrypt_cbc will handle the padding.
     int ciphertext_len = encrypt_cbc(decoded_string, decoded_len, key, iv, ciphertext+16);
@@ -28,11 +28,12 @@ int create_token_server()
     {
         ciphertext[i] = iv[i];
     }
+    FILE *fp_out = fopen("token.txt", "w");
     //print the ciphertext for now.  update later to write it to a file or something.
     for(i=0; i<(ciphertext_len+16); i++)
     {
-        printf("%02x\n", ciphertext[i]);
+        fprintf(fp_out,"%02X", ciphertext[i]);
     }
-    
+    fclose(fp_out);
     return 0;
 }
